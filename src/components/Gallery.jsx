7cc10d6148665854c147739782e81b2a246a67f0
Matt Mandelbaum <mattrmandelbaum@gmail.com>
2022-03-11 18:09:27 -0500

    aa

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,23 +1,36 @@
-import React from "react"
+import React, { useState } from "react"
 
 import tennisImage from "../images/TennisImage.JPG"
 import tennisImage2 from "../images/TennisImage2.JPG"
+import jeffrey from "../images/IMG-3669.jpg"
+
+const images = [tennisImage, tennisImage2, jeffrey]
 
 function Gallery(){
-    return <div className="galleryContainer">
+
+    const [imageToShow, setImageToShow] = useState('')
+
+    const imageElements = images.map((image) => (
+        <div className="gallery" >
+            <img className={imageToShow === image ? "active" : "inactive"} src={image} onClick={() => showImage(image)}/>
+        </div>
+    )) 
+
+    const showImage = (image) => {
+        setImageToShow(image)
+    }
+
+    return <div className="fullGallery">
+            <div className="galleryContainer">
                 <div className="galleryTitle">
                     <h2 className="galleryTitle">Tennis</h2>
                 </div>
                 <div className="galleryImages">
-                    <div className="gallery">
-                        <img src={tennisImage} alt="Description One"/>
-                    </div>
-
-                    <div className="gallery">
-                        <img src={tennisImage2} alt="Description Two"/>
-                    </div>
+                    {imageElements}
                 </div>
-    </div>
+            </div>
+            
+            </div>
 }
 
 export default Gallery
